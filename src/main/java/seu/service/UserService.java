3a517cba764955c59493f2db9070953e3ccfa519
jayshawn <jayshawn@qq.com>
2019-04-11 13:48:38 +0800

    验证码

--- src/main/java/seu/service/UserService.java
+++ src/main/java/seu/service/UserService.java
@@ -4,9 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import seu.base.CodeCaptchaServlet;
 import seu.base.CodeEnum;
 
 import seu.dao.LoginTicketDAO;
